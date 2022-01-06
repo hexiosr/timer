d836b161ed9466eedafa7a8d6efd237e41090b6c
hexios <969497645@qq.com>
2022-01-06 17:25:58 +0800

    update logging

--- timer.py
+++ timer.py
@@ -7,32 +7,34 @@ from urllib import request
 import time
 import datetime
 import ssl
+import logging_config
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-print(' 开始')
+logger = logging_config.Config().get_config()
+
+logger.info(' 开始')
 
 
 def fun():
-    now_time = datetime.datetime.now()
-    print('------------------------')
-    print(now_time)
     try:
         with request.urlopen(
                 "http://81.68.209.220/"
         ) as file:
             if file.status == 200:
-                print('不发短信')
-    except:
-        print('发短信')
+                logger.info(file.status)
+                logger.info(file.read())
+                logger.info('不发短信 ')
+    except Exception as e:
+        logger.info(' 发短信')
+        logger.info(e)
 
 
-# 定义sleep函数
 def sleep(hour, min, sec):
     return hour * 3600 + min * 60 + sec
 
 
-# 定时执行
+# 定时
 seconds = sleep(0, 0, 1)
 print()
 while True:
