e0fdb30a75e15035d4d77256641d246963a517fc
hexios <969497645@qq.com>
2022-01-06 12:00:29 +0800

    update

--- timer.py
+++ timer.py
@@ -4,14 +4,29 @@
 # @File    : timer.py
 
 from urllib import request
+import time
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
-try:
-    with request.urlopen(
-            "https://www.baidu.com/"
-    ) as file:
-        print(file.status)
-        print(file.reason)
-except Exception as e:
-    print(e)
+
+def fun():
+    try:
+        with request.urlopen(
+                "https://www.baidu.com/"
+        ) as file:
+            print(file.status)
+            print(file.reason)
+            print('-------')
+    except Exception as e:
+        print(e)
+
+
+def sleep(hour, min, sec):
+    return hour * 3600 + min * 60 + sec
+
+
+seconds = sleep(0, 0, 3)
+while 1 == 1:
+    time.sleep(seconds)
+    fun()
