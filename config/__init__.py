6b1c8fe8ae109e7e69b7183d1c905a3954b99fa0
hexiosr <hiskying@icloud.com>
2022-03-23 15:32:32 +0800

    改下格式

---
+++ config/__init__.py
@@ -0,0 +1,20 @@
+# @Author  : hexios
+# @Email   : hiskying@icloud.com
+# @Time    : 2022/3/23 15:26
+# @File    : __init__.py
+
+# 短信配置
+# Your Account SID from twilio.com/console
+account_sid = "xxxxx"
+# Your Auth Token from twilio.com/console
+auth_token = "xxxx"
+
+# 邮件配置
+# 发件人邮箱账号
+mail_user = 'xxxx@qq.com'
+# 发件人邮箱密码，为授权码一般为非登录密码
+mail_pass = 'xxxx'
+# 设置接收邮件
+receivers = ['xxxxx@icloud.com']
+# 邮件内容
+mail_msg = "系统出现异常："
--- timer.py
+++ timer.py
@@ -3,14 +3,17 @@
 # @Author  : hexios
 # @Email   : hexiosr@icloud.com
 # @File    : timer.py
-
+import smtplib
 import ssl
 import time
+from email.header import Header
+from email.mime.text import MIMEText
 from urllib import request
 
-import logging_config
-import sms
-import smtp
+from twilio.rest import Client
+
+import config
+from config import logging_config
 
 ssl._create_default_https_context = ssl._create_unverified_context
 logger = logging_config.Config().get_config()
@@ -19,7 +22,7 @@ logger = logging_config.Config().get_config()
 def fun():
     try:
         with request.urlopen(
-                "http://81.68.209.220:25555/1111.html"
+                "http://81.68.209.220:25555/1111.html"  # 接口
         ) as file:
             if file.status == 200:
                 print('正常')
@@ -27,9 +30,31 @@ def fun():
                 logger.info(file.read())
     except Exception as e:
         print('异常')
-        smtp.emails()  # 邮件
-        sms.sms1()  # 短信
+
+        # 邮件提醒
+        # 邮件内容，文本格式，把plain改成html是html格式
+        message = MIMEText(config.mail_msg + f'{e}!', 'html', 'utf-8')
+        # 显示的发件人
+        message['From'] = config.mail_user
+        # 显示的收件人
+        message['To'] = ','.join(config.receivers)
+        message['Subject'] = Header('系统出现异常！', 'utf-8')
+        smtp = smtplib.SMTP_SSL('smtp.qq.com', 465)
+        smtp.login(config.mail_user, config.mail_pass)
+        smtp.sendmail(config.mail_user, message['To'].split(','), message.as_string())
+        smtp.quit()
+        logger.info("邮件发送成功！")
+
+        # 短信提醒
+        client = Client(config.account_sid, config.auth_token)
+        message = client.messages.create(
+            to="+xxxx",
+            from_="+xxxx",
+            body="系统出现异常：" + f'{e} !')
+        logger.info("短信发送成功！")
         logger.info(e)
+        print(message.sid)
+        print("短信，邮件发送成功！")
 
 
 # 定时函数
